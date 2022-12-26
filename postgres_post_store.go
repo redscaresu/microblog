9e1e9bab19773ded3c4fd58de1dae94959242054
redscaresu <ukashouri@googlemail.com>
2022-12-26 01:04:42 +0000

    intial db config

---
+++ postgres_post_store.go
@@ -0,0 +1,27 @@
+package microblog
+
+import (
+	"context"
+	"database/sql"
+)
+
+type PostgresStore struct {
+	DB *sql.DB
+}
+
+func (p *PostgresStore) GetAll() ([]string, error) {
+	// horrible SQL goes here
+	// handle errors, etc
+	return posts, nil
+}
+
+func (ps *PostgresStore) Retrieve(ID string) (Widget, error) {
+	w := Widget{}
+	ctx := context.Background()
+	row := ps.DB.QueryRowContext(ctx, "SELECT id, name FROM widgets WHERE id = ?", ID)
+	err := row.Scan(&w.ID, &w.Name)
+	if err != nil {
+		return Widget{}, err
+	}
+	return w, nil
+}
