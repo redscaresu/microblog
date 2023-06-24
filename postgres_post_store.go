c354c3562a52d9cecab917ce252cf56123d39424
redscaresu <ukashouri@googlemail.com>
2023-06-24 11:30:30 +0100

    feat: works locally and in the cloud now

--- postgres_post_store.go
+++ postgres_post_store.go
@@ -23,8 +23,22 @@ func New() *PostgresStore {
 	// psqlInfo := fmt.Sprintf("%s:%s@unix(/%s)/%s?parseTime=true",
 	// 	user, password, unixSocketPath, dbName)
 
-	psqlInfo := fmt.Sprintf("user=%s password=%s database=%s host=%s",
+	local := os.Getenv("LOCAL")
+
+	var psqlInfo string
+
+	psqlInfo = fmt.Sprintf("user=%s password=%s database=%s host=%s",
 		user, password, dbName, unixSocketPath)
+
+	if local == "local" {
+		host := os.Getenv("HOST")
+		port := os.Getenv("PORT")
+		password := os.Getenv("PASSWORD")
+		user := os.Getenv("USER")
+		psqlInfo = fmt.Sprintf("host=%s port=%s user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbName)
+	}
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
