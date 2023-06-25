f89ae8245b71386d2ef7a14a8027d2cb8be86043
redscaresu <ukashouri@googlemail.com>
2023-06-25 10:26:05 +0100

    feat: add protected and unprotected handlers

--- microblog_handler.go
+++ microblog_handler.go
@@ -1,6 +1,8 @@
 package microblog
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"embed"
 	"fmt"
 	"html/template"
@@ -16,58 +18,81 @@ var (
 	templates embed.FS
 )
 
+type application struct {
+	auth struct {
+		username string
+		password string
+	}
+	poststore PostStore
+}
+
+func (app *application) protectedHandler(w http.ResponseWriter, r *http.Request) {
+	err := RenderHTMLTemplate(w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w)
+}
+
+func (app *application) unprotectedHandler(w http.ResponseWriter, r *http.Request) {
+	posts, err := app.poststore.GetAll()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+	fmt.Fprint(w, posts)
+}
+
+func (app *application) basicAuth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if ok {
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte(app.auth.username))
+			expectedPasswordHash := sha256.Sum256([]byte(app.auth.password))
+
+			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+			if usernameMatch && passwordMatch {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	})
+}
+
 func ListenAndServe(addr string, ps PostStore) error {
-	//pass netListener as string not netListener object
+
+	app := new(application)
+	app.poststore = ps
+
+	app.auth.username = os.Getenv("AUTH_USERNAME")
+	app.auth.password = os.Getenv("AUTH_PASSWORD")
+
+	if app.auth.username == "" {
+		log.Fatal("basic auth username must be provided")
+	}
+
+	if app.auth.password == "" {
+		log.Fatal("basic auth password must be provided")
+	}
+
 	customMux := http.NewServeMux()
 
-	// customMux.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
-	// 	userPassword := r.FormValue("password")
-	// 	if IsAuthenticated(userPassword) {
-	// 		bg := &BlogPost{Blog_Id: int(uuid.New().ID()), Blog_Post: r.FormValue("blog")}
-	// 		log.Println(bg)
-	// 		err := ps.Create(*bg)
-	// 		if err != nil {
-	// 			w.WriteHeader(http.StatusInternalServerError)
-	// 			fmt.Fprint(w, err)
-	// 			return
-	// 		}
-	// 		w.WriteHeader(http.StatusCreated)
-	// 		fmt.Fprint(w, "awesome blog post")
-	// 		return
-	// 	}
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	fmt.Fprint(w, "access denied")
-	// })
-
-	customMux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		posts, err := ps.GetAll()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, err)
-			return
-		}
-		fmt.Fprint(w, posts)
-	})
-
-	customMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// posts, err := ps.GetAll()
-		// if err != nil {
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	fmt.Fprint(w, err)
-		// 	return
-		// }
-
-		err := RenderHTMLTemplate(w)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprint(w)
-	})
+	customMux.HandleFunc("/unprotected", app.unprotectedHandler)
+	customMux.HandleFunc("/protected", app.basicAuth(app.protectedHandler))
 
 	err := http.ListenAndServe(addr, customMux)
 	fmt.Println(err)
 	return err
+
 }
 
 func RenderHTMLTemplate(w io.Writer) error {
