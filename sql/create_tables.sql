b2797a8abec69dc6944558aa9ca5eafceb1e8c56
redscaresu <ukashouri@googlemail.com>
2023-10-03 12:56:39 +0100

    chore: cleanup

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,7 @@
 -- Creation of blog table
 CREATE TABLE IF NOT EXISTS blog (
   blog_id uuid NOT NULL,
-  blog_title varchar(10000) NOT NULL,
-  blog_post varchar(10000) NOT NULL,
+  blog_title TEXT NOT NULL,
+  blog_post TEXT NOT NULL,
   PRIMARY KEY (blog_id)
 );
