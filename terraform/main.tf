2d812c0e765ae4a35250e51c337008a0155a59cf
redscaresu <ukashouri@googlemail.com>
2025-04-04 16:33:14 +0100

    my blog

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,26 +4,27 @@ resource scaleway_container_namespace main {
     provider = scaleway.p2
 }
 
-# resource scaleway_container main {
-#     provider = scaleway.p2
-#     name = "my-container-02"
-#     description = "environment variables test"
-#     namespace_id = scaleway_container_namespace.main.id
-#     registry_image = "${scaleway_container_namespace.main.registry_endpoint}/alpine:test"
-#     port = 9997
-#     cpu_limit = 140
-#     memory_limit = 256
-#     min_scale = 1
-#     max_scale = 1
-#     timeout = 600
-#     privacy = "private"
-#     protocol = "http1"
-#     deploy = true
+resource scaleway_container main {
+    provider = scaleway.p2
+    name = "blog"
+    description = "my blog"
+    namespace_id = scaleway_container_namespace.main.id
+    registry_image = "${scaleway_container_namespace.main.registry_endpoint}/microblog:latest"
+    port = 9997
+    cpu_limit = 70
+    memory_limit = 128
+    min_scale = 1
+    max_scale = 1
+    timeout = 600
+    privacy = "public"
+    protocol = "http1"
+    deploy = true
+    http_option = "redirected"
 
-#     environment_variables = {
-#         "foo" = "var"
-#     }
-#     secret_environment_variables = {
-#       "key" = "secret"
-#     }
-# }
+    environment_variables = {
+        "foo" = "var"
+    }
+    secret_environment_variables = {
+      "key" = "secret"
+    }
+}
