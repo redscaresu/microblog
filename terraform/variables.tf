832546d335b6463f9b231d53ea99979cd8b624c1
redscaresu <ukashouri@googlemail.com>
2025-06-21 19:11:27 +0100

    fix: pipeline test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,9 @@ variable "project_id" {
   description = "The Scaleway project ID"
   type        = string
 }
+
+variable "container_image_tag" {
+  description = "The container image tag to deploy"
+  type        = string
+  default     = "latest"
+}
